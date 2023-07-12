7a7d46bba3ed5c0286916c370380a65ada9863ef
unknown <mreshnaan7@gmail.com>
2023-07-12 12:56:46 +0530

    add error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,20 @@
-
 function Error() {
   return (
-    <div>Error</div>
-  )
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "100vh",
+        width: "100vw",
+        color: "black",
+      }}
+    >
+      <h1>404 Page Not Found</h1>
+      <h6>Error</h6>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
